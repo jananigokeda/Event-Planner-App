bf15b2ba63e7a6e8f9a45ff188a974dbe4445785
Prishanthy <anga0016@algonquinlive.com>
2025-03-27 20:33:29 -0400

    Update database.dart

--- lib/database.dart
+++ lib/database.dart
@@ -1,17 +1,13 @@
 import 'dart:async';
 import 'customer_dao.dart';
 import 'customer_item.dart';
-import 'vehicle_dao.dart';
 import 'package:floor/floor.dart';
 import 'package:sqflite/sqflite.dart' as sqflite;
-
 import 'vehicle_item.dart';
-
 part 'database.g.dart'; // Generated code
 
-@Database(version: 1, entities: [VehicleItem,CustomerItem])
+@Database(version: 1, entities: [CustomerItem])
 abstract class AppDatabase extends FloorDatabase {
-  VehicleDao get vehicleDao; // Add the DAO getter
   CustomerDAO get customerDAO;
 }
 
